3a79ea8e11508d5341204ac6e9bd43860e987a72
Verania Calderon <102995385+veraniacalderon@users.noreply.github.com>
2022-05-01 23:12:15 -0700

    Update PyPoll_Challenge(1).py

--- PyPoll_Challenge(1).py
+++ PyPoll_Challenge(1).py
@@ -5,7 +5,7 @@
 import csv
 import os
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("..", "Resources/election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join( "analysis", "election_analysis.txt")
 
